db30799610d668911fecc9a138af1c0987bf6664
Muhammad Arslan <imarslan7061@gmail.com>
2025-01-27 23:45:55 +0500

    initial template

---
+++ app.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let create = document.createElement("div")
+    create.classList.add("container")
+    create.innerHTML = `<p>Todo</p>`
+})
